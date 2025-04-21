1260fbe3a8798f3e65cca1c7d86a61c549229fb6
jatin <pasijatin2004@gmail.com>
2025-04-21 17:33:15 +0530

    commit message

---
+++ Variables&DataTypes1/Average.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+    int Avg = (a+b)/2;
+    cout<<"The average is:"<<Avg<<endl;
+    return 0;
+}
---
+++ Variables&DataTypes1/input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+    int sum = a + b;
+    int product = a * b;
+    cout<<"The sum is = "<<sum<<endl;
+    cout<<"The Product is = "<<product<<endl;
+    return 0;
+}
